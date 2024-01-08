bf7484d6f51c9817fc66b42a4150995cdd322222
cheekeong84 <cktan@microsoft.com>
2024-01-08 22:40:36 +0800

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "app_name" {
+  type        = string
+  description = "name of the app"
+  default     = "hobbescktan123"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of Resources"
+  default     = "westeurope"
+}
